f8034fbff9abd314719e6cfd098531d9e7b538a4
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-10-08 17:32:43 -0300

    canto consume this api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Habilita o CORS para todas as rotas
 
   app.useGlobalPipes(
     new ValidationPipe({
